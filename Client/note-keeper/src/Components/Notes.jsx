d34d1d5a9a5a68aac3c072701b60d0237a448f51
Durgesh Kumar Singh <dk829445@gmail.com>
2022-08-13 02:19:01 +0530

    homepage done

--- Client/note-keeper/src/Components/Notes.jsx
+++ Client/note-keeper/src/Components/Notes.jsx
@@ -1,14 +1,48 @@
-import { Typography, Row } from 'antd';
+import { useState } from 'react';
+import { Row, Col, Skeleton, Modal, Typography, message, Card } from 'antd';
+import axios from 'axios';
 import './Styles/Notes.styles.css'
+import { useEffect } from 'react';
 const { Title, Text } = Typography;
 export const Notes = () => {
+    const [notes, setNotes] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(()=> {
+        getNotes();
+    },[])
+    const getNotes = () => {
+        setLoading(true);
+        axios.get(`https://notes-keeper-v1.herokuapp.com/`).then((res) => {
+            if(res?.status === 200){
+                setLoading(false);
+                setNotes(res?.data);
+            }
+        }).catch((err) => {
+            setLoading(false);
+            message.err(`Oops! Something went wrong. Server did not respond.`)
+            console.log(err);
+        })
+    }
     return (<>
-        <Row>
-        <Title align='center' level={1}>Welcome To Note Keeper</Title>
-        </Row>
-        <Row align='center'>
-        <Text style={{textAlign:"center"}} className='header-subtext'>Add your notes here and customize as per your convenience</Text>
-        </Row>
-
+    <Row align='center'>
+        <Title level={2}>Welcome To Notes Keeper 2.0</Title>
+    </Row>
+    <Row align='center'>
+        <Text>Here you can store all of your notes and information in an easy-to-use outline and can quickly find what you need.</Text>
+    </Row><br/>
+    {loading ? <Row>
+        <Skeleton/>
+    </Row> : 
+    <Row className='cards-container'>
+        {notes?.map((item) => {
+            return (<Col className='cards-col'>
+            <Card hoverable title={item?.title}>
+                <Text>Tagline : {item?.tagline}</Text>
+            </Card>
+            </Col>)
+        })}
+    </Row>
+    }
+      
     </>)
 }
