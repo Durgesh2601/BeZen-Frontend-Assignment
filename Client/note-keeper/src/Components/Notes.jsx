6654cd91743bda42310ee52579bc730c0d1f9fe5
Durgesh Kumar Singh <dk829445@gmail.com>
2022-08-13 15:57:22 +0530

    notes card UI

--- Client/note-keeper/src/Components/Notes.jsx
+++ Client/note-keeper/src/Components/Notes.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Row, Col, Skeleton, Modal, Typography, message, Card } from 'antd';
+import { PushpinOutlined, PushpinFilled } from '@ant-design/icons';
 import axios from 'axios';
 import './Styles/Notes.styles.css'
 import { useEffect } from 'react';
@@ -7,6 +8,7 @@ const { Title, Text } = Typography;
 export const Notes = () => {
     const [notes, setNotes] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [isPinned, setIsPinned] = useState(false);
     useEffect(()=> {
         getNotes();
     },[])
@@ -23,6 +25,27 @@ export const Notes = () => {
             console.log(err);
         })
     }
+    const checkIsPinned = (id) => {
+        if(notes){
+            const newNote = notes?.filter((element) => element?._id === id);
+            if(newNote[0]?.isPinned){
+                return <PushpinFilled size={22} onClick={() => togglePin(newNote[0]?._id, newNote[0]?.isPinned)} />
+            } else {
+                return <PushpinOutlined size={20} onClick={() => togglePin(newNote[0]?._id, newNote[0]?.isPinned)}/>
+            }
+            
+        }
+    }
+    const togglePin = (id, status) => {
+       axios.patch(`https://notes-keeper-v1.herokuapp.com/pin/${id}`, {isPinned : !status}).then((res) => {
+        if(res?.status === 200) {
+            message.success(`Note pinned successfully`);
+        }
+       }).catch((err) => {
+        message.err(`Oops! Something went wrong`);
+        console.log(err)
+       })
+    }
     return (<>
     <Row align='center'>
         <Title level={2}>Welcome To Notes Keeper 2.0</Title>
@@ -36,8 +59,12 @@ export const Notes = () => {
     <Row className='cards-container'>
         {notes?.map((item) => {
             return (<Col className='cards-col'>
-            <Card hoverable title={item?.title}>
-                <Text>Tagline : {item?.tagline}</Text>
+            <Card hoverable title={item?.title}
+            extra={checkIsPinned(item?._id)}>
+                <Row>
+                <Text><strong>Tagline</strong> : {item?.tagline}</Text>
+                </Row>
+                <Text><strong>Description</strong> : {item?.body}</Text>
             </Card>
             </Col>)
         })}
